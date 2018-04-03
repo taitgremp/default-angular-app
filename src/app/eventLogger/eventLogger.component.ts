54b1e7fc20a2393b630572d7559f6b5b185540cc
GabrielTait <taitgremp@gmail.com>
2018-04-02 22:30:55 -0400

    Assignment3

--- src/app/eventLogger/eventLogger.component.ts
+++ src/app/eventLogger/eventLogger.component.ts
@@ -3,11 +3,29 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-event-logger',
   styles: [`
-    
+    .pastFive {
+      color: white;
+    }
   `],
   templateUrl: './eventLogger.component.html'
 })
 
 export class EventLoggerComponent {
 
+  textHidden = true;
+  btnClicks = [];
+  pastFiveClicks = [];
+
+  doEventLog() {
+    this.textHidden = !this.textHidden;
+    if (this.btnClicks.length < 5) {
+      this.btnClicks.push(new Date());
+    } else {
+      this.pastFiveClicks.push(new Date());
+    }
+  }
+
+  getColor() {
+    return this.btnClicks.length > 4 ? 'blue' : 'white';
+  }
 }
