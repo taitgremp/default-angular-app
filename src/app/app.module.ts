fd8b1a121f82d9bbcee448cdb896dd4e570369e6
GabrielTait <taitgremp@gmail.com>
2018-04-02 21:58:32 -0400

    Assignment3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,13 @@ import { FormsModule} from '@angular/forms';
 import { HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { ServerComponent} from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
+import {EventLoggerComponent} from './eventLogger/eventLogger.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent
+    EventLoggerComponent
   ],
   imports: [
     FormsModule,
---
+++ src/app/eventLogger/eventLogger.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-event-logger',
+  styles: [`
+    
+  `],
+  templateUrl: './eventLogger.component.html'
+})
+
+export class EventLoggerComponent {
+
+}
