c7a310b327add562a289236928e363b3599b2db1
GabrielTait <taitgremp@gmail.com>
2018-04-05 22:27:34 -0400

    Course Project Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms';
-import { HttpModule} from '@angular/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ShoppingListComponent} from './shopping-list/shopping-list.component';
+import {ShoppingListEditComponent} from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+import {RecipeDetailComponent} from './recipe-book/recipe-detail/recipe-detail-component';
+import {RecipeItemComponent} from './recipe-book/recipe-list/recipe-item/recipe-item.component';
+import {RecipeListComponent} from './recipe-book/recipe-list/recipe-list.component';
+import {HeaderComponent} from './header/header.component';
+import {RecipeBookComponent} from './recipe-book/recipe-book.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeListComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeBookComponent
   ],
   imports: [
     FormsModule,
---
+++ src/app/recipe-book/recipe-detail/recipe-detail-component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: './recipe-detail-component.html',
+  styleUrls: ['./recipe-detail-component.css'],
+  selector: 'app-recipe-detail'
+})
+
+export class RecipeDetailComponent {
+
+}
